14717bbee43b5ac66264b2faada8e6e5f7e3d275
wbrink <will.brink55@gmail.com>
2021-11-30 22:49:48 -0800

    can now get to the sso button

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ public static class Selenium
 {
     static void Main() 
     {
+
         using (IWebDriver driver = new ChromeDriver(".\\"))
         {
             string url = @"https://libraries.tusd.net/";
@@ -19,6 +20,15 @@ public static class Selenium
             // click the link to be taken to link with sso button
             link.Click();
 
+            // get the next login button
+            IWebElement loginLink2 = driver.FindElement(By.CssSelector("[href='/common/servlet/presentloginform.do?fromLoginLink=true']"));
+            loginLink2.Click();
+
+            IWebElement ssoLogin = driver.FindElement(By.ClassName("idpLoginButton"));
+            ssoLogin.Click();
+
+
+            Console.WriteLine("Username: {0}", Environment.UserDomainName);
             // loginButton.Click(); // sends us the sso form
             // Console.WriteLine(loginButton);
             Console.ReadKey();
